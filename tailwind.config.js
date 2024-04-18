8443b6a4e139d37bbfb6900cf5e27745cc9f6c37
Abolfazl Edalati <a.g.e.tech@outlook.com>
2024-04-18 14:24:40 +0330

    build(TalwindCSS): add main colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        accent: "#183D3D",
+        "accent-shade": "#0D2323",
+        neutral: "#A7D397",
+      },
+      colors: {
+        accent: "#2F2F2F",
+        "accent-tint": "#646464",
+        "accent-primary": "#183D3D",
+        "accent-secondary": "#A7D397",
+        neutral: "#fff",
+        "neutral-tint": "#F3F3F3",
+      },
+      padding: {
+        normal: "12px 24px",
+      },
+    },
+    borderRadius: {
+      xl: "8px",
+    },
   },
   plugins: [],
 };
