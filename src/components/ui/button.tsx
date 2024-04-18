35fd9642ef49b2e912d8716aa2dc1ff3339b5fb9
Abolfazl Edalati <a.g.e.tech@outlook.com>
2024-04-18 15:30:52 +0330

    feat<ui>: add border to Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,7 +8,7 @@ export default function Button({ children, className, href }: Props) {
       href={href}
       className={[
         className,
-        "text-accent-primary text-[15px] bg-neutral p-normal rounded-xl transition-all duration-200 transi hover:bg-accent hover:text-accent-secondary",
+        "text-accent-primary text-[15px] bg-neutral p-normal rounded-xl border border-neutral transition-all duration-200 transi hover:bg-accent hover:text-accent-secondary ",
       ].join(" ")}
     >
       {children}
