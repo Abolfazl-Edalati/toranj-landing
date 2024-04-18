7c18e7443d95adaf06c1f78edb6078f2c79f5072
Abolfazl Edalati <a.g.e.tech@outlook.com>
2024-04-18 14:56:39 +0330

    fix(ui): fix bug => change button to anchor

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-type Props = React.ComponentPropsWithoutRef<"button">;
+type Props = React.ComponentPropsWithoutRef<"a">;
 
-export default function Button({ children, className }: Props) {
+export default function Button({ children, className, href }: Props) {
   return (
-    <button
+    <a
+      href={href}
       className={[
         className,
         "text-accent-primary bg-neutral p-normal rounded-xl transition-all duration-200 transi hover:bg-accent hover:text-accent-secondary",
       ].join(" ")}
     >
       {children}
-    </button>
+    </a>
   );
 }
