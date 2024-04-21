fbf9f51a96a83010773707c4b433a73847e5308b
Abolfazl Edalati <a.g.e.tech@outlook.com>
2024-04-21 20:08:23 +0330

    fix: fix columns

--- src/components/customers/customers.tsx
+++ src/components/customers/customers.tsx
@@ -19,20 +19,17 @@ export default function Customers() {
     },
   ];
 
-  const columns = Math.min(customers.length, 4);
-  const gridTemplateColumns = `repeat(${columns}, 1fr)`;
-
   return (
     <div className="flex flex-col gap-16 mb-32 max-[970px]:mb-20 max-[880px]:gap-10 max-[620px]:mb-16 max-[550px]:gap-6">
       <span className="text-4xl text-accent font-bold text-center max-[880px]:text-3xl max-[550px]:text-2xl">
         مشتریان قالب رستوران و کافه ترنج
       </span>
       <div
-        className={`grid grid-cols-${columns} gap-y-24 overflow-auto px-28 max-[1250px]:grid-cols-3 max-[980px]:px-0 max-[680px]:grid-cols-2`}
-        style={{ gridTemplateColumns: gridTemplateColumns }}
+        className={`grid grid-cols-4 gap-y-24 overflow-auto px-28 max-[1250px]:grid-cols-3 max-[980px]:px-0 max-[680px]:grid-cols-2`}
       >
         {customers.map((c, index) => (
           <SingleCustomer
+            key={index}
             title={c.title}
             image={c.image}
             href={c.href}
